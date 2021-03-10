99fa05ad94b0579b9d505565768583c56809aba0
Aleksander_Salyga <asalygabusiness@gmail.com>
2021-03-10 21:49:12 +0100

    Update main.py

--- days_21-30/day_24_script/main.py
+++ days_21-30/day_24_script/main.py
@@ -5,7 +5,7 @@ nato_dic = pd.read_csv("nato_phonetic_alphabet.csv",
                        index_col=0,
                        header=None,
                        squeeze=True).to_dict()
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+# TODO 2. Create a list of the phonetic code words from a word that the user inputAs.
 word_name = input("Please Type The word: ").upper()
 
 user_word_nato = [nato_dic[letter] for letter in word_name]
