1d5f5574288543a6e9510ac94d511a546c8077a0
Aleksander_Salyga <asalygabusiness@gmail.com>
2021-03-29 14:42:45 +0200

    Add Exception Handling

--- days_21-30/day_24_script/main.py
+++ days_21-30/day_24_script/main.py
@@ -5,8 +5,18 @@ nato_dic = pd.read_csv("nato_phonetic_alphabet.csv",
                        index_col=0,
                        header=None,
                        squeeze=True).to_dict()
-# TODO 2. Create a list of the phonetic code words from a word that the user inputAs.
-word_name = input("Please Type The word: ").upper()
 
-user_word_nato = [nato_dic[letter] for letter in word_name]
-print(user_word_nato)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user input:
+def translate():
+    word_name = input("Please Type The word: ").upper()
+    try:
+        user_word_nato = [nato_dic[letter] for letter in word_name]
+    except KeyError:
+        print("Sorry, only letter in alphabet please.")
+        translate()
+    else:
+        print(user_word_nato)
+
+
+translate()
